ba4527ddc1da0786d21351698a6f5d5af15d289d
STEPHEN <sashioyas@gmail.com>
2025-01-17 17:03:48 +0300

    first commit

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,7 @@ return [
     'consumer_key' => 'AhnfmrRF8Gw6InD4WbmFCRGm6pvEMpfVZYNDXuMsdg3rh22z',
     'consumer_secret' => 'MFn6fuTGnBnz8S9lKjVua9EfqgbKODRczhoAcyx3pqob8kTCA41XPQIRysVO03jA',
     'passkey' => 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
-    'shortcode' => '174379', // Safaricom Test PayBill/Shortcode
+    'shortcode' => '9854077', // Safaricom Test PayBill/Shortcode
     'callback_url' => 'https://moneyglowup-mpesa1-backend.vercel.app/api/callback', // Change to your live callback URL
 ];
 ?>
