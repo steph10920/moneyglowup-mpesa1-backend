54cc3f3519f214a16681e7a1ec79557006ed820c
STEPHEN <sashioyas@gmail.com>
2025-01-17 17:08:29 +0300

    first commit

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: https://moneyglowup-by-brenda.vercel.app");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 
