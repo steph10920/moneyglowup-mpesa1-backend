8a537d091a489a53639f1862e5562115b972082e
STEPHEN <sashioyas@gmail.com>
2025-01-17 16:57:31 +0300

    first commit

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://moneyglowup-by-brenda.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $requestUri = explode('?', $_SERVER['REQUEST_URI'])[0];
 
